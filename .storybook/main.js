392e37f6625b0fad0f90015dae64b73a11ddcef3
Mary Salvi <mary.salvi@kitware.com>
2022-03-15 16:40:59 -0400

    adjust configurations

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,9 @@
+// const Vue = require('@vitejs/plugin-vue');
+// const Vuetify = require('@vuetify/vite-plugin');
+const { loadConfigFromFile, mergeConfig } = require("vite");
+const eslintPlugin = require("vite-plugin-eslint").default;
+const path = require("path");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -5,10 +11,22 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
   ],
   "framework": "@storybook/vue3",
   "core": {
     "builder": "storybook-builder-vite"
-  }
-}
+  },
+  async viteFinal(previousConfig) {
+    const { config } = await loadConfigFromFile(
+      path.resolve(__dirname, "../vite.config.ts")
+    );
+
+    return mergeConfig(previousConfig, {
+      ...config,
+      plugins: [
+        eslintPlugin(),
+      ],
+    });
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+// import { withVuetify } from '@socheatsok78/storybook-addon-vuetify/dist/decorators'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +9,23 @@ export const parameters = {
     },
   },
 }
+
+// export const globalTypes = {
+//   theme: {
+//     name: 'Theme',
+//     description: 'Global theme for components',
+//     defaultValue: 'light',
+//     toolbar: {
+//       icon: 'circlehollow',
+//       items: ['light', 'dark']
+//     }
+//   }
+// }
+
+export const decorators = [
+  (story) => (
+    ({
+      template: '<v-app><story/></v-app>',
+    })
+  )
+]
