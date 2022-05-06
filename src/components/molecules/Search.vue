994508fbbed946c7ab75e635168e35d4f760a959
Mary Salvi <mary.salvi@kitware.com>
2022-05-05 22:43:29 -0400

    add search component

---
+++ src/components/molecules/Search.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+import {
+  defineComponent,
+} from 'vue';
+import {
+  updateResults,
+} from '@/store/search';
+
+export default defineComponent({
+  name: 'Search',
+  setup(){
+    return {
+      updateResults,
+    };
+  }
+});
+</script>
+
+<template>
+  <v-card
+    flat
+    color="blue-grey darken-4"
+  >
+    <v-form
+      @submit.prevent="updateResults()"
+    >
+      <v-row justify="center">
+        <v-col cols="11">
+          <v-card-subtitle>Specify search area</v-card-subtitle>
+          <!-- <GeoJsonForm /> -->
+          <v-card-subtitle>Apply search logic</v-card-subtitle>
+          <!-- <Predicate /> -->
+          <v-card-subtitle>Acquired date range</v-card-subtitle>
+          <!-- <DateRange /> -->
+
+          <v-btn
+            color="primary"
+            block
+            x-large
+            type="submit"
+            class="mt-6"
+          >
+            Search
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-card>
+</template>
