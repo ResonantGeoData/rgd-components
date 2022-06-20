0051082f6fd8bcb134e2b633873212a55712eb48
Mary Salvi <mary.salvi@kitware.com>
2022-06-20 13:36:34 -0400

    add predicate component

---
+++ src/components/atoms/Predicate.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import {
+  defineComponent,
+} from 'vue';
+
+import { searchParameters } from '@/store/search';
+
+export default defineComponent({
+  name: 'Predicate',
+  emits: {'input': null},
+  setup() {
+    const predicate = [
+      'within',
+      'contains',
+      'crosses',
+      'disjoint',
+      'equals',
+      'intersects',
+      'overlaps',
+      'touches',
+    ];
+
+    return {
+      predicate,
+      searchParameters,
+    };
+  },
+});
+
+</script>
+
+<template>
+  <v-select
+    v-model="searchParameters.predicate"
+    :items="predicate"
+    label="Predicate"
+    outlined
+    dense
+    value=""
+    @input="$emit('input', searchParameters)"
+  />
+</template>
